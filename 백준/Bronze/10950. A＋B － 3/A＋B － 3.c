d3f09b1025eddb5fe34fc43b141b23ec4ffc078e
JIMINLEEE <snsdvip0607@gmail.com>
2026-04-16 19:54:38 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+    int a ,b;
+
+    scanf("%d", &num);
+    for(int i =0; i<num; i++){
+        scanf("%d %d", &a, &b);
+
+        printf("%d\n", a+b);
+    }
+
+}
